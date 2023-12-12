4d621c5d1a0210bd5123e7ff6fcf1f1996f8575b
sedraxnet <sedraxnet@gmail.com>
2023-12-12 09:44:16 +0100

    First Committ

--- cmd/genkeypair/main.go
+++ cmd/genkeypair/main.go
@@ -12,7 +12,7 @@ func main() {
 		panic(err)
 	}
 
-	privateKey, publicKey, err := libsedraxwallet.CreateKeyPair(false)
+	privateKey, publicKey, err := libkaspawallet.CreateKeyPair(false)
 	if err != nil {
 		panic(err)
 	}
